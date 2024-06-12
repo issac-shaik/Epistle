e8f53b0f2de1a8b8f0dcac4ecd33e0abdea55d70
Issac Shaik <issac.shaik@gmail.com>
2024-06-12 17:28:54 +0530

    final commit (hopefully)

---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,124 @@
+import { Hono } from "hono";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client/edge";
+import { verify } from "hono/jwt";
+import { createBlogInput, updateBlogInput } from "@issac-shaik/epistle-common";
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  try {
+    const user = await verify(authHeader, c.env.JWT_SECRET);
+    if (user) {
+      c.set("userId", user.id);
+      await next();
+    }
+  } catch (error) {
+    c.status(403);
+    return c.json({
+      message: "Inputs are not correct",
+    });
+  }
+});
+
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
+  const { success } = createBlogInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({ error: "Inputs not valid" });
+  }
+
+  const authorId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blog = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId: Number(authorId),
+    },
+  });
+  return c.json({
+    id: blog.id,
+  });
+});
+blogRouter.put("/update", async (c) => {
+  const body = await c.req.json();
+  const { success } = updateBlogInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({ error: "Inputs not correct" });
+  }
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const userId = c.get("userId");
+  try {
+    const blog = await prisma.post.update({
+      where: {
+        id: body.id,
+        authorId: Number(userId),
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+
+    return c.json(blog);
+  } catch (error) {
+    console.error(error);
+    return c.json({
+      error: "Cannot update the blog, you're not the owner",
+    });
+  }
+});
+
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blogs = await prisma.post.findMany();
+
+  return c.json({
+    blogs,
+  });
+});
+
+blogRouter.get("/:id", async (c) => {
+  const id = c.req.param("id");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const blog = await prisma.post.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+    return c.json({
+      blog,
+    });
+  } catch (e) {
+    c.status(411);
+    return c.json({
+      message: "Error while fetching blog post",
+    });
+  }
+});
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,80 @@
+import { Hono } from "hono";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client/edge";
+import { sign, verify } from "hono/jwt";
+import { signupInput, signinInput } from "@issac-shaik/epistle-common";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const body = await c.req.json();
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
+    c.status(400);
+    return c.json({
+      error: "Username or password passed did not contain valid inputs",
+    });
+  }
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+      },
+    });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    console.error(e);
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  } finally {
+    await prisma.$disconnect();
+  }
+});
+
+userRouter.post("/signin", async (c) => {
+  const body = await c.req.json();
+  const { success } = signinInput.safeParse(body);
+
+  if (!success) {
+    c.status(400);
+    return c.json({
+      error: "Username or password passed did not contain valid inputs",
+    });
+  }
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: body.username,
+        password: body.password,
+      },
+    });
+    if (!user) {
+      c.status(403);
+      return c.json({
+        msg: "user not found",
+      });
+    }
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    console.log(e);
+    c.status(403);
+    return c.json({ error: "error while signing in" });
+  }
+});
